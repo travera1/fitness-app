ecf4c692b07ab4fdcd49ca7e630a37f0f4d412ca
Aaron Traver <travera1@hawkmail.newpaltz.edu>
2021-11-28 18:57:25 -0500

    no longer using our config file to read variables in sever.js file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const { PORT } = require('../config')
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
 const path = require('path')
 
 //for production build

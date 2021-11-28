4492220acb47d0c740ae3db2f119cc7a1c46156d
Aaron Traver <travera1@hawkmail.newpaltz.edu>
2021-11-28 18:53:29 -0500

    imports fixed to remove error

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,8 +16,8 @@
 </style>
 
 <script>
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 export default {
   components: {
     Navbar, Footer
